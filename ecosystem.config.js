e1fa94d98d207da57b54c1ea778e044dff29f5ac
nannn <2337286751@qq.com>
2021-11-17 21:53:36 +0800

    modify pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       host: '8.218.210.218', // 服务器ip地址
       ref: 'origin/master', // 要拉取代码的分支
       repo: 'git@gitee.com:nnnannn/vuecil_weather.git', // 代码仓库地址，我存放的是 Github
-      path: '/workspace/weather', // 拉取代码后，存放在服务器的文件地址。
+      path: '/www/wwwroot/weather', // 拉取代码后，存放在服务器的文件地址。
       'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && npm run build && pm2 startOrReload ecosystem.config.js', // 顺序执行这些指令，最后 pm2 startOrReload ecosystem.config.js 会通过 pm2 启动 vue3-admin-server.js
       env: {
         NODE_ENV: 'production'
