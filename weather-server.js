19fb1081136c295fbd7dc1111694f73824f0a0b7
nannn <2337286751@qq.com>
2021-11-17 21:52:03 +0800

    modify pm2

--- weather-server.js
+++ weather-server.js
@@ -1,6 +1,9 @@
-const server = require('pushstate-server')
-
-server.start({
-  port: 5018,
-  directory: './dist'
+const express = require('express');
+const path = require('path');
+const compression = require('compression');
+const app = express();
+app.use(compression())
+app.use(express.static(path.join(__dirname, '/dist')))
+app.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
 })
